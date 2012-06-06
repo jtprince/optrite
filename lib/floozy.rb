cc346fb071b0420bc7fb4fad23564c0c5a245139
John Prince <jtprince@gmail.com>
2012-06-06 01:41:33 -0600

    basic features in place

--- lib/floozy.rb
+++ lib/floozy.rb
@@ -1,25 +1,67 @@
 
+require 'optparse'
+
 # copied *liberally* from micro-optparse.  See LICENSE.txt for more detailed.
 
 class Floozy
 
-  Option = Struct.new(:name, :desc, :settings) 
+  # typically nil or false
+  DEFAULT_BLANK = nil
 
-  attr_accessor :banner, :version
+  Option = Struct.new(:name, :desc, :settings)
+
+  attr_writer :banner, :version
   # a hash of symbol => value that can be used to fill the defaults 
-  attr_accessor :defaults
+  attr_writer :defaults
+  # the underlying OptionParser object
+  attr_accessor :option_parser
+
+  # sets the banner attribute with a usage line using the script name
+  # File.basename($0).  Returns the current banner attribute
+  def usage(args_and_such=nil)
+    @banner = "usage: #{File.basename($0)}"
+    (@banner << ' ' << args_and_such) if args_and_such
+    @banner
+  end
+
+  # allows getting or setting based on whether an argument was provided
+  def banner(arg=nil)
+    arg.nil? ? @banner : (@banner = arg)
+  end
+
+  # allows getting or setting based on whether an argument was provided
+  def defaults(arg=nil)
+    arg.nil? ? @defaults : (@defaults = arg)
+  end
+
+  # allows getting or setting based on whether an argument was provided
+  def version(arg=nil)
+    arg.nil? ? @version : (@version = arg)
+  end
+
   def initialize(&block)
-    @options = []
+    @opts_etc = []
     @used_short = []
     @defaults = {}
     @default_values = nil
+    @option_parser = OptionParser.new
     block.call(self) if block
   end
 
-  def option(name, desc, settings = {})
-    @options << Option.new(name, desc, settings)
+  # returns only the options out of the opts_etc things
+  def options
+    @opts_etc.select {|obj| obj.is_a?(Option) }
+  end
+
+  def option(name, desc="", settings = {})
+    @opts_etc << Option.new(name, desc, settings)
   end
   alias_method :opt, :option
+
+  def text(arg="")
+    @opts_etc << arg
+  end
+  alias_method :separator, :text
 
   def short_from(name)
     name.to_s.each_char.find do |char|
@@ -27,10 +69,30 @@ class Floozy
     end
   end
 
-  def validate(options)
-    options.each_pair do |key, value|
-      opt = @options.find_all{ |option| option.name == key }.first
-      key = "--" << key.to_s.gsub("_", "-")
+  def name_to_longopt(name, boolean=false)
+    (boolean ? "--[no-]" : "--") << name.to_s.gsub("_", "-")
+  end
+
+  def type_to_class(arg)
+    return arg if arg.is_a?(Class)
+    case arg.to_s
+    when /bool/
+      FalseClass
+    when /int/
+      Integer
+    when /float/
+      Float
+    when /string/
+      String
+    else
+      raise "invalid type: #{arg}"
+    end
+  end
+
+  def validate(opt_hash)
+    opt_hash.each_pair do |key, value|
+      opt = options.find{|option| option.name == key }
+      key = name_to_longopt(key)
       unless opt.settings[:value_in_set].nil? || opt.settings[:value_in_set].include?(value)
         puts "Parameter for #{key} must be in [" << opt.settings[value_in_set].join(", ") << "]" ; exit(1)
       end
@@ -43,37 +105,52 @@ class Floozy
     end
   end
 
+  def to_s
+    @option_parser.to_s
+  end
+  alias_method :educate, :to_s
+  alias_method :help, :to_s
+
   def process!(args = ARGV)
     @result = (@default_values || {}).clone # reset or new
-    @optionparser ||= OptionParser.new do |op| # prepare only once
-      @options.each do |option|
+    op = @option_parser
+    @opts_etc.each do |thing|
+      if thing.is_a?(String)
+        op.separator(thing) 
+      else
+        option = thing
         short = (option.settings[:short] || short_from(option.name))
         @used_short << short
-        default = option.settings[:default] || @defaults[option.name] || false # set default
+        default = option.settings[:default] || @defaults[option.name] || DEFAULT_BLANK # set default
         @result[option.name] = default         
-        klass = ( Fixnum===default ? Integer : default.class )
+        klass = 
+          if option.settings[:type]
+            type_to_class(option.settings[:type]) 
+          else
+            Fixnum.===(default) ? Integer : default.class
+          end
 
         if [TrueClass, FalseClass, NilClass].include?(klass) # boolean switch
-          op.on("-" << short, "--[no-]" << option.name.to_s.gsub("_", "-"), option.desc) {|v| @result[option.name] = v}
+          op.on("-" << short, name_to_longopt(option.name, true), option.desc) {|v| @result[option.name] = v}
         else # argument with parameter
-          op.on("-" << short, "--" << option.name.to_s.gsub("_", "-") << " " << default.to_s, klass, option.desc) {|x| @result[option.name] = x}
+          op.on("-" << short, name_to_longopt(option.name) << " " << default.to_s, klass, option.desc) {|x| @result[option.name] = x}
         end
       end
-
-      op.banner = @banner unless @banner.nil?
-      op.on_tail("-h", "--help", "Show this message") {puts op ; exit}
-      short = @used_short.include?("v") ? "-V" : "-v"
-      op.on_tail(short, "--version", "Print version") {puts @version ; exit} unless @version.nil?
-      @default_values = @result.clone # save default values to reset @result in subsequent calls
     end
 
+    op.banner = @banner unless @banner.nil?
+    op.on_tail("-h", "--help", "Show this message") {puts op ; exit}
+    short = @used_short.include?("v") ? "-V" : "-v"
+    op.on_tail(short, "--version", "Print version") {puts @version ; exit} unless @version.nil?
+    @default_values = @result.clone # save default values to reset @result in subsequent calls
+
     begin
-      @optionparser.parse!(args)
+      @option_parser.parse!(args)
     rescue OptionParser::ParseError => e
       puts e.message ; exit(1)
     end
 
-    validate(@result) if self.respond_to?("validate")
+    validate(@result) if self.respond_to?(:validate)
     @result
   end
 
--- spec/floozy_spec.rb
+++ spec/floozy_spec.rb
@@ -14,10 +14,10 @@ describe Floozy do
         p.opt :chance, "set mutation chance", :default => 0.8, :value_satisfies => lambda {|x| x >= 0.0 && x <= 1.0}
       end
       options = fl.process!([])
-      options.should == {:severity=>4, :verbose=>false, :mutation=>"MightyMutation", :plus_selection=>true, :selection=>"BestSelection", :chance=>0.8}
+      options.should == {:severity=>4, :verbose=>nil, :mutation=>"MightyMutation", :plus_selection=>true, :selection=>"BestSelection", :chance=>0.8}
     end
 
-    it 'allows all defaults to be set at once, not minding if they are over specified' do
+    it 'allows all defaults to be set at once, not minding if there are extra keys' do
       defaults = {
         :severity => 4, 
         :selection => 'BestSelection', 
@@ -32,20 +32,77 @@ describe Floozy do
       options.should == {:severity=>4, :selection=>"BestSelection"}
     end
 
-    it 'can set type without giving a default' do
+    it 'can set the option type without giving a default' do
+      fl = Floozy.new do |p|
+        p.opt :intopt1, "", :type => Integer
+        p.opt :intopt2, "", :type => :int
+        p.opt :intopt2n, "", :type => :int
+        p.opt :stringopt1, "", :type => String
+        p.opt :stringopt2, "", :type => :string
+        p.opt :stringopt2n, "", :type => :string
+        p.opt :boolopt0, ""  # or boolean (although type is assumed boolean if none given
+        p.opt :boolopt1, "", :type => :bool  # or boolean (although type is assumed boolean if none given
+        p.opt :boolopt2, "", :type => TrueClass
+        p.opt :boolopt2n, "", :type => TrueClass
+      end
+      options = fl.process!(%w(--intopt1 2 --intopt2 3 --stringopt1 3 --stringopt2 happy --boolopt0 --boolopt1 --no-boolopt2) )
+      options.should == {:intopt1=>2, :intopt2=>3, :intopt2n=>nil, :stringopt1=>"3", :stringopt2=>"happy", :stringopt2n=>nil, :boolopt0=>true, :boolopt1=>true, :boolopt2=>false, :boolopt2n=>nil}
+
+    end
+
+    it 'can auto-generate a usage line' do
+      fl = Floozy.new do |p|
+        p.usage
+        p.opt :boolopt2, "", :type => TrueClass
+      end
+      fl.process! []  # <-- I shouldn't have to process things to look at the complete help message!
+      fl.to_s.should =~ /^usage: rspec\n/
+
+      fl = Floozy.new do |p|
+        p.usage "[OPTIONS] <file1> <file2>"
+        p.opt :wildness
+      end
+      fl.process!([])
+      fl.to_s.should =~ /^usage: rspec \[OPTIONS\] <file1> <file2>/
+    end
+
+    it 'allows the addition of text lines (for things like option grouping)' do
+      fl = Floozy.new do |p|
+        p.banner = "This is a fancy script, for usage see below"
+        p.text "[I just like my space here]"
+        p.version = "fancy script 0.0 alpha"
+        p.text  # can be given no args
+        p.text "WEIRD OPTIONS: "
+        p.opt :severity, "set severity", :default => 4, :value_in_set => [4,5,6,7,8]
+        p.opt :verbose, "enable verbose output"
+        p.text ""
+        p.separator "EVEN STRANGER OPTIONS:"  # also can call separator
+        p.opt :plus_selection, "use plus-selection if set", :default => true
+        p.opt :selection, "selection used", :default => "BestSelection", :short => "l"
+      end
+      options = fl.process!([])
+      options.should == {:severity=>4, :verbose=>nil, :plus_selection=>true, :selection=>"BestSelection"}
+      fl.to_s.should =~ /This is a fancy/
+      fl.to_s.should =~ /WEIRD OPTIONS: /
+      fl.to_s.should =~ /EVEN STRANGER OPTIONS:/
+    end
+
+    it 'can cast and validate arguments' do
+
     end
 
     it 'can do subcommands' do
+
     end
 
-    it 'allows option grouping' do
+    it 'formats to fit terminal size, even on windows'
+    it 'can cast items handed in inside an array'
+
+    it 'is contained entirely within one file' do
+      Dir[File.dirname(__FILE__) + "/../lib/*"].size.should == 1
     end
 
-    it 'is contained in one file' do
-    end
 
-    it 'formats to fit terminal size, even on windows' do
-    end
   end
 
 end
